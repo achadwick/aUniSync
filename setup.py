73bdd9eab8d67945e8a4bd90296227fef4321b5f
Andrew Chadwick <a.t.chadwick@gmail.com>
2013-05-23 01:48:55 +0100

    Add custom status icons
    Icons use the Symbolic style introduced by GNOME and partly supported by
    GTK+. Use a hack to get the status icon looking right.

--- setup.py
+++ setup.py
@@ -6,13 +6,25 @@ setup(
     name="aunisync",
     version="0.0.1",
     scripts=["aunisync"],
-    data_files=[("share/applications", ["aunisync.desktop"])],
+    data_files=[
+        ("share/applications", [
+                "aunisync.desktop",
+            ]),
+        ("share/icons/hicolor/scalable/status", [
+                "icons/status/aunisync-idle-symbolic.svg",
+                "icons/status/aunisync-changed-symbolic.svg",
+                "icons/status/aunisync-active-symbolic.svg",
+                "icons/status/aunisync-properties-symbolic.svg",
+                "icons/status/aunisync-offline-symbolic.svg",
+                "icons/status/aunisync-paused-symbolic.svg",
+                "icons/status/aunisync-error-symbolic.svg",
+            ]),
+    ],
     author="Andrew Chadwick",
     author_email="a.t.chadwick@gmail.com",
     fullname="Automatic Unison Synchronizer",
     description="Automatically synchronizes files and folders in Unison profiles whenever they're modified",
     long_description="""
-
 aUniSync is a low-profile interface to the Unison file synchronizer which
 presents as an icon in the system tray, runs in the background, and tries to
 stay out of your way.  It calls unison in batch, non-GUI mode shortly after you
@@ -22,7 +34,6 @@ For changes to files on the remote side, aUniSync uses a regular polling
 strategy.  It is network aware, and restarts synchronization when the network
 is available.  Synchronization can be paused for all profiles or just a subset
 by running Unison's GUI from the tray icon.
-
 """,
     keywords=["unison", "synchronizing", "sync", "replication", "dropbox-like",
               "inotify", "system tray", "status icon"],
