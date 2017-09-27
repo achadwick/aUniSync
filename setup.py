18e43d4a325cd5671e36f4fd1fbf7e1eb9b85c30
Andrew Chadwick <a.t.chadwick@gmail.com>
2017-09-27 21:25:31 +0100

    Version bump and flake8 stuff.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.version_info < (3,):
 
 setup(
     name="aunisync",
-    version="0.2.1",
+    version="0.2.2",
     scripts=["aunisync"],
     data_files=[
         ("share/applications", [
@@ -29,7 +29,10 @@ setup(
     author="Andrew Chadwick",
     author_email="a.t.chadwick@gmail.com",
     fullname="Automatic Unison Synchronizer",
-    description="Automatically synchronizes files and folders in Unison profiles whenever they're modified",
+    description=(
+        "Automatically synchronizes files and folders described "
+        "in Unison profiles whenever they're modified."
+    ),
     long_description="""
 aUniSync is a low-profile interface to the Unison file synchronizer which
 presents as an icon in the system tray, runs in the background, and tries to
@@ -41,8 +44,12 @@ strategy.  It is network aware, and restarts synchronization when the network
 is available.  Synchronization can be paused for all profiles or just a subset
 by running Unison's GUI from the tray icon.
 """,
-    keywords=["unison", "synchronizing", "sync", "replication", "dropbox-like",
-              "inotify", "system tray", "status icon"],
-    license="License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    keywords=[
+        "unison", "synchronizing", "sync", "replication",
+        "dropbox-like", "inotify", "system tray", "status icon",
+    ],
+    license=(
+        "License :: OSI Approved "
+        ":: GNU General Public License v3 or later (GPLv3+)"
+    ),
 )
-
